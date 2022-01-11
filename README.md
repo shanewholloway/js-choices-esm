a54fcd91a4a007b29d5a450c01dcc04c0c47d621
Shane Holloway <shane.holloway@ieee.org>
2022-01-11 11:23:25 -0700

    Initial transpile of choices.js typescript into ES Module using Rollup

---
+++ README.md
@@ -0,0 +1,22 @@
+# Choices-esm.js 
+
+An ES Modules build of [Choices.js](https://github.com/jshjohnson/Choices#readme) via Rollup and TypeScript.
+
+## Use
+
+```js
+import Choices, {templates} from 'choices-esm';
+```
+
+## Updated Build
+
+1. Update dependencies in `package.json`, particularly `"choices.js"`
+2. `npm install`
+3. `npm build`
+4. `npm publish`
+
+
+## License
+
+[MIT License](./LICENSE) -- same as [Choices.js/LICENSE]()
+
---
+++ rollup.config.js
@@ -0,0 +1,22 @@
+import rpi_resolve from '@rollup/plugin-node-resolve'
+import rpi_commonjs from '@rollup/plugin-commonjs'
+import { terser as rpi_terser } from 'rollup-plugin-terser'
+
+const _cfg_ = {
+  plugins: [rpi_resolve(), rpi_commonjs()] }
+
+const _cfg_min_ = { ... _cfg_,
+  plugins: [... _cfg_.plugins, rpi_terser()] }
+
+export default [
+  { ... _cfg_, input: 'dist/index.js',
+    output: [
+      { file: `esm/choices-esm.js`, format: 'esm', sourcemap: true},
+    ]},
+
+  { ... _cfg_min_, input: 'dist/index.js',
+    output: [
+      { file: `esm/choices-esm.min.js`, format: 'esm', sourcemap: false},
+    ]},
+]
+
