e89b3750f489976868b677d2815e120442e13504
Shane Holloway <shane.holloway@ieee.org>
2022-01-11 21:00:16 -0700

    Fix use of process.env by transcluded redux. Added index.html as validation and demo

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,21 @@
 import rpi_resolve from '@rollup/plugin-node-resolve'
+import rpi_replace from '@rollup/plugin-replace'
 import rpi_commonjs from '@rollup/plugin-commonjs'
 import { terser as rpi_terser } from 'rollup-plugin-terser'
 
 const _cfg_ = {
-  plugins: [rpi_resolve(), rpi_commonjs()] }
+  plugins: [
+    rpi_resolve(),
+
+    rpi_commonjs(), // allow use of deepmerge
+
+    rpi_replace({
+      preventAssignment: true,
+      values: { // fix redux use of process.env.NODE_ENV
+        'process.env.NODE_ENV': JSON.stringify('production')
+      },
+    }),
+  ]}
 
 const _cfg_min_ = { ... _cfg_,
   plugins: [... _cfg_.plugins, rpi_terser()] }
@@ -19,4 +31,3 @@ export default [
       { file: `esm/choices-esm.min.js`, format: 'esm', sourcemap: false},
     ]},
 ]
-
